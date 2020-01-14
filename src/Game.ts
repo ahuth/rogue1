88e8a0aecfb44efd3a391a9f37690f0184cc14da
Andrew Huth <andrew@huth.me>
2020-01-13 21:47:43 -0800

    Draw a tiny arena

--- src/Game.ts
+++ src/Game.ts
@@ -4,10 +4,19 @@ export default class {
   display: ROT.Display;
 
   constructor() {
-    this.display = new ROT.Display();
+    this.display = new ROT.Display({ width: 10, height: 5 });
   }
 
   init(container: Element) {
     container.appendChild(this.display.getContainer()!);
+    this.drawMap();
+  }
+
+  private drawMap() {
+    const map = new ROT.Map.Arena(10, 5);
+
+    map.create((x, y, wall) => {
+      this.display.draw(x, y, wall ? '#' : '.', 'white', 'black');
+    });
   }
 }
