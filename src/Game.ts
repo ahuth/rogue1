e247eaf112fbe192b0e1a4489f60810be1023833
Andrew Huth <andrew.huth@airbnb.com>
2020-01-26 17:19:46 -0800

    Display a more interesting map

--- src/Game.ts
+++ src/Game.ts
@@ -1,17 +1,28 @@
 import * as ROT from 'rot-js';
 
+const height = 20;
+const width = 80;
+
 export default class Game {
-  display = new ROT.Display({ width: 10, height: 5 });
+  display = new ROT.Display({ width, height });
 
   getElement(): HTMLElement {
     return this.display.getContainer()!;
   }
 
   draw(): void {
-    const map = new ROT.Map.Arena(10, 5);
+    const generator = new ROT.Map.Cellular(width, height);
+    generator.randomize(0.5);
 
-    map.create((x, y, wall) => {
-      this.display.draw(x, y, wall ? '#' : '.', 'white', 'black');
+    // Smooth the map
+    for (let i = 0; i < 3; i += 1) {
+      generator.create();
+    }
+
+    generator.create((x, y, wall) => {
+      this.display.draw(x, y, wall ? '#' : ' ', 'white', 'black');
     });
+
+    generator.connect(() => {}, 0);
   }
 }
